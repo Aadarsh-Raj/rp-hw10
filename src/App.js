33c14dee56ce97dbbe4aa1d80fda67b12f739d0b
Aadarsh Raj <aadarshraj06062001@gmail.com>
2024-01-08 14:00:41 +0530

    added folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import _ from 'lodash';
 
 function App() {
+const [data, setData] =useState("Loading");
+const [keyWord, setKeyWord] = useState("Loading");
+useEffect(()=>{
+  try {
+    query({"inputs": keyWord}).then((response) => {
+      const imageUrl = URL.createObjectURL(response);
+      setData(imageUrl);
+      console.log(imageUrl);
+    });
+  } catch (error) {
+
+    console.log(error);
+  }
+},[keyWord]);
+
+
+
+
+  async function query(d) {
+    try {
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/prompthero/openjourney-v4",
+        {
+          headers: { Authorization: "Bearer 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" },
+          method: "POST",
+          body: JSON.stringify(d),
+        }
+      );
+      const result = await response.blob();
+            return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const debouncedAPICall = _.debounce((e) => {
+    console.log("Arya");
+    getValue(e);
+  }, 200);
+
+
+function getValue(e){
+const value = e.target.value;
+console.log(value);
+setKeyWord(value);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <main className='main' style={{width:'100vw', height:'100vh', display:'flex',flexDirection:"column", justifyContent:'center', alignItems:"center", gap:"1rem"}}>
+    <input type="text" placeholder='Enter any keyword' onChange={(e)=>debouncedAPICall(e)} /> <br/>
+    <img src={data} alt="" />
+    </main>
+    </>
   );
 }
 
